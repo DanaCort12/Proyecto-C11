73e359fa6bc5d01148c13161d008d691c8f8e463
DanaCort12 <109707791+DanaCort12@users.noreply.github.com>
2022-09-14 22:01:42 -0500

    Add files via upload

---
+++ Actividad-de-la-maestra-C11-main/sketch.js
@@ -0,0 +1,13 @@
+var marks = [30, 40, 45, 35, 10, 55, 65, 60, 90, 95, 20, 25];
+for (var f = 0; f < marks.lenght; f = f + 1){
+  console.log (marks [f])
+}
+
+function setup() {
+  createCanvas(400, 400);
+
+}
+
+function draw() {
+  background(150);
+}
